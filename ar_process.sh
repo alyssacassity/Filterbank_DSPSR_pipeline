ebd7ce451f9826dbdcd43eee40c939bcbbc365fa
Alyssa Cassity <cassityalyssa@gmail.com>
2025-09-25 16:35:18 -0700

    Update ar_process.sh

--- ar_process.sh
+++ ar_process.sh
@@ -3,9 +3,10 @@
 pam -f 128 -T -p -d 0. -e ftp *.ar
 
 # psredit DM back to real value
-psredit -c dm=19.5 -e fix *.ftp
+DM = 19.5
+psredit -c dm=$DM -e fix *.ftp
 
-# smooth 
+# smooth profile
 psrsmooth -W *.fix
 
 
